769e1a750adb4220b7dc8dd0bed47b153c3b2c6e
Paul Willoughby <paul.willoughby@signal-ai.com>
2020-05-21 13:14:54 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Minimal React App
+
+This is intended as the most minimal way to get started with React. It aims to be much lighter weight than [create-react-app][] and is focused on basic config and compilation, for example it has no opinions about linting, testing, deployment or monitoring.
+
+## Get started
+
+```
+$ yarn
+$ yarn build
+$ yarn start
+```
+
+Then go to http://localhost:5000.
+
+For dev mode with hot reloading use:
+
+```
+$ yarn dev-start
+```
+
+Then go to http://localhost:8081.
+
+In dev mode you don't need to run the `yarn build` step.
+
+# In the Box
+
+* React 16
+* Babel 7 config
+* Webpack 4 config
+* Tested with Node 12.x, may work with other versions.
+
+[create-react-app]:https://github.com/facebook/create-react-app
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'Minimal React App',
+      template: path.resolve(__dirname, 'src', 'templates', 'index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'dist'),
+        transformPath(targetPath, absolutePath) {
+          return targetPath.replace(/^assets\//, '');
+        }
+      }]
+    }),
+  ]
+};
